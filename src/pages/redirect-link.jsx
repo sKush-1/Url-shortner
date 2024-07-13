fe694ebbf2f475aed1ec8522ba3554ad3c4d6d9d
skush-1 <kushsumit27@gmail.com>
2024-07-13 18:00:29 +0530

    add stats for clicks

--- src/pages/redirect-link.jsx
+++ src/pages/redirect-link.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import { storeClicks } from "@/db/apiClicks";
+import { getLongUrl } from "@/db/apiUrls";
+import useFetch from "@/hooks/use-fetch";
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { BarLoader } from "react-spinners";
 
 const RedirectLink = () => {
-  return (
-    <div>
-      redirct
-    </div>
-  )
-}
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const { loading, data, fn } = useFetch(getLongUrl, id);
+  const { loading: loadingStats, fn: fnStats } = useFetch(storeClicks, {
+    id: data?.id,
+    originalUrl: data?.original_url,
+  });
 
-export default RedirectLink
+  useEffect(() => {
+    fn();
+  }, []);
+
+  useEffect(() => {
+    if (!loading && data) {
+      fnStats();
+    }
+  }, [loading]);
+  
+  if (data?.original_url) {
+    window.location.href = data.original_url;
+  }
+
+  if (loading || loadingStats) {
+    return (
+      <>
+        <BarLoader width={"100%"} color="#36d7b7" />
+        <br />
+        Redirecting...
+      </>
+    );
+  }
+
+  return null;
+};
+
+export default RedirectLink;
